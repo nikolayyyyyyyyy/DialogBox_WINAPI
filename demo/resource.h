362e1118f491de1f1320110968e75c5c06ebefd5
Nikolay Nikolaev <cholyo999999@gmail.com>
2025-03-17 21:30:00 +0200

    Add project files.

---
+++ demo/resource.h
@@ -0,0 +1,41 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by demo.rc
+//
+#define IDC_MYICON                      2
+#define IDD_DEMO_DIALOG                 102
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_DEMO                        107
+#define IDI_SMALL                       108
+#define IDC_DEMO                        109
+#define IDR_MAINFRAME                   128
+#define IDD_MY_FIRST_DIALOG             129
+#define IDC_BUTN_CHECKEGN               1000
+#define IDC_EDIT_EGN                    1001
+#define IDC_SEX_BOX                     1002
+#define IDC_AGE_BOX                     1003
+#define IDC_RADIO_MALE                  1004
+#define IDC_RADIO_FEMALE                1005
+#define IDC_RADIO_CHILD                 1006
+#define IDC_RADIO_ADULT                 1007
+#define IDC_CHECK_RO                    1008
+#define IDC_CHECK2                      1009
+#define IDC_CHECK_HIDDEN                1009
+#define IDD_ABOUTBOX                    1031
+#define ID_MYFIRSTDIALOG_MYFIRST        32771
+#define ID_FILE_MYFIRSTDIALOG           32773
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
